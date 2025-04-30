6274905b5324860a537b9e71e699d6f2de3e85e6
TuhitsWeb987 <tuhiti.falchetto.web@gmail.com>
2025-04-30 10:34:58 -1000

    Rename HomePage in Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { HeroSection } from "@/components/HeroSection";
-import { HowWorkSection } from "@/components/HowWorkSection";
-import { LocationFamousSection } from "@/components/LocationFamousSection";
-import { Section } from "@/components/Section";
+import { HeroSection } from "@/components/HeroSection/HeroSection";
+import { HowWorkSection } from "@/components/HowWorkSection/HowWorkSection";
+import { LocationFamousSection } from "@/components/LocationFamousSection/LocationFamousSection";
+import { Section } from "@/components/Commons/Section";
 
-export default function HomePage() {
+export default function Page() {
+
   return (
     <Section className="flex flex-col items-center justify-center w-full">
       <HeroSection />
