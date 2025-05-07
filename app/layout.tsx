ef826663ed7d166313e26dbe269bea1c25905f9a
TuhitsWeb987 <tuhiti.falchetto.web@gmail.com>
2025-05-06 21:41:08 -1000

    update main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full bg-[#FFF5E5]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-[#FFF5E5]`}
       >
         <Header />
         <ToastProvider />
